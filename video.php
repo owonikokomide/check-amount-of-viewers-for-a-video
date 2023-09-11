4108b8e42c586283e242b5d2fd8bd8558033391a
owonikoko Ayomide <donaykoko@yahoo.com>
2023-09-11 14:53:48 +0100

    Update video.php

--- video.php
+++ video.php
@@ -1,11 +1,12 @@
 <?php include('config.php'); ?>
 <?php
 if(isset($_POST['play'])){
-    $select = "SELECT * FROM link WHERE id = '1'";
+    $id = $_POST['hide'];
+    $select = "SELECT * FROM link  WHERE id ='$id'";
     $select_query = mysqli_query($connection, $select);
     $row = mysqli_fetch_array($select_query);
     $r = $row['views']+1;
-$update = mysqli_query($connection,"UPDATE link SET views ='$r' WHERE id ='1'");
+$update = mysqli_query($connection,"UPDATE link SET views ='$r' WHERE id ='$id'");
 if($update){
 //  echo "succesful";
 }
@@ -18,31 +19,54 @@ if($update){
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>link count</title>
+
 </head>
 <body>
   <div class="container">
-  <?php
-    $select = "SELECT * FROM link WHERE id = '1'";
- $select_query = mysqli_query($connection, $select);
- $row = mysqli_fetch_array($select_query);
- $r = $row['video_name'];
-        ?>
-  <form action="" method="post">
 
- <h2><span>Video</span> Player</h2>
+    <table>
+<tr>
+<!-- <th>Video name</th> -->
+<th>Views</th>
+<th>Video</th>
+<th>Play</th>
+</tr>
 
+    <?php
+    $select = "SELECT * FROM link";
+    $select_query = mysqli_query($connection, $select);
+     while($e=mysqli_fetch_array($select_query)){
+    ?>
+<tr>
+    <!-- <td><?php echo $e['id']; ?></td> -->
+    <!-- <td><?php echo $e['video_name']; ?></td> -->
+    <td><?php echo $e['views']." views"; ?></td>
+    <td>
     <video id="video" width="640" height="360" controls>
-     
-    <source src="vid/<?php echo $r?>" type="video/mp4">
-    </video>
-</br>
+    <source src="vid/<?php echo $e['video_name']; ?>" type="video/mp4">
+     </video>
+    </td>
+    <td>
+    <form method="post">
+ 
+        <input type="hidden" name="hide" value="<?php echo $e['id']; ?>" >
+    <button type="submit" name="play" id="play-button" class="but">Play</button>
+     </form>
+    </td>
+</tr>
 
-        <button type="submit" name="play" id="play-button" class="but">Play</button>
+<?php
+     }
+?>
+    </table>
+</div>
+   
+
 
   </form>
 
     </div>
-
+  
     <script>
         var video = document.getElementById("video");
         var playButton = document.getElementById("play-button");
