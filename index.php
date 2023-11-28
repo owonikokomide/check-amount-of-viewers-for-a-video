62ca5b3e0c00beacee725480bf05e29431e7c3e6
owonikoko Ayomide <donaykoko@yahoo.com>
2023-11-28 17:31:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>upload video</title>
-</head>
+</head> 
 <body>
   <div class="container">
     <h1>Upload a video</h1>
