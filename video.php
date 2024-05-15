4f47828dde980d9bd44073631aff46c74bf2d48b
owonikoko Ayomide <donaykoko@yahoo.com>
2024-05-15 13:40:19 +0100

    Update video.php

--- video.php
+++ video.php
@@ -14,7 +14,7 @@ if($update){
 ?>
 <!DOCTYPE html> 
 <html lang="en">
-<head>
+<head> 
   <link rel="stylesheet" href="video.css">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
